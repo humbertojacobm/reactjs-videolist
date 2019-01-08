f9779d5b337c13c5111af8b6806d1a06c0c2296f
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-01-07 23:37:40 -0500

    formularios

---
+++ src/widget/components/search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './search.css'
+
+const Search = () => (
+    <form className="Search">
+      <input 
+        type="text"
+        placeholder="Busca tu video favorito" 
+        className="Search-input"/>
+    </form>
+)
+
+export default Search;
